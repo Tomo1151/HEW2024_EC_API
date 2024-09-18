c85b3bc461123056345960fdf93004b67adac1af
Tomo <94550718+Tomo1151@users.noreply.github.com>
2024-09-18 13:29:13 +0900

    filtering returned user object

--- src/users.ts
+++ src/users.ts
@@ -6,10 +6,20 @@ const prisma = new PrismaClient();
 
 app.get("/", async (ctx) => {
   try {
-    const users = await prisma.user.findMany();
-    return ctx.json({success: true, data: users, length: users.length}, 200);
-  } catch {
-    return ctx.json({success: false, error: "Failed to fetch users"}, 500);
+    const users = await prisma.user.findMany({
+      select: {
+        username: true,
+        nickname: true,
+        bio: true,
+        homepage_link: true,
+        icon_link: true,
+        created_at: true,
+      },
+    });
+    return ctx.json({ success: true, data: users, length: users.length }, 200);
+  } catch (e) {
+    console.log(e);
+    return ctx.json({ success: false, error: "Failed to fetch users" }, 500);
   }
 });
 

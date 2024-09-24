da3e01031361b6e5e7067603324f0894b47c7d30
Tomo <94550718+Tomo1151@users.noreply.github.com>
2024-09-24 09:27:45 +0900

    add token expire response

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -1,37 +1,39 @@
-import { Context, MiddlewareHandler, Next } from 'hono';
-import { getCookie } from 'hono/cookie';
-import { verify } from 'hono/jwt';
+import { Context, Next } from "hono";
+import { getCookie } from "hono/cookie";
+import { verify } from "hono/jwt";
 
-
-async function isAuthenticated (c: Context, next: Next) {
-    try {
-        if (!Bun.env.JWT_SECRET) {
-            throw new Error("JWT_SECRET_KEY is not set");
-        }
-
-        const token = getCookie(c, "access_token");
-        const refreshToken = getCookie(c, "refresh_token");
-
-        if (!token) {
-            return c.json({ message: "You do not have permission" }, 401);
-        }
-
-        try {
-            const decoded = await verify(token, Bun.env.JWT_SECRET, 'HS256');
-
-            if (!decoded) {
-                return c.json({ message: "You do not have permission" }, 401);
-            }
-
-            c.set("jwtPayload", decoded);
-            await next();
-        } catch (error) {
-            console.log(error);
-            return c.json({ message: "You do not have permission" }, 401);
-        }
-    } catch (error) {
-            return c.json({ message: "You do not have permission" }, 401);
+async function isAuthenticated(c: Context, next: Next) {
+  try {
+    if (!Bun.env.JWT_SECRET) {
+      throw new Error("JWT_SECRET_KEY is not set");
     }
+
+    const token = getCookie(c, "access_token");
+    const refreshToken = getCookie(c, "refresh_token");
+
+    if (!token) {
+      if (refreshToken) {
+        return c.json({ message: "Token expired" }, 401);
+      }
+      return c.json({ message: "You do not have permission" }, 401);
+    }
+
+    try {
+      const decoded = await verify(token, Bun.env.JWT_SECRET, "HS256");
+
+      if (!decoded) {
+        return c.json({ message: "You do not have permission" }, 401);
+      }
+
+      c.set("jwtPayload", decoded);
+      await next();
+    } catch (error) {
+      console.log(error);
+      return c.json({ message: "You do not have permission" }, 401);
+    }
+  } catch (error) {
+    return c.json({ message: "You do not have permission" }, 401);
+  }
 }
 
 export default isAuthenticated;

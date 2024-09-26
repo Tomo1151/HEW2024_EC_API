ee28a7f76c0d18f16abff600e5d1eee60fa147bb
Tomo <94550718+Tomo1151@users.noreply.github.com>
2024-09-26 12:25:34 +0900

    add create post endpoint

--- src/posts.ts
+++ src/posts.ts
@@ -1,16 +1,67 @@
+import { zValidator } from "@hono/zod-validator";
 import { PrismaClient } from "@prisma/client";
 import { Hono } from "hono";
+import { z } from "zod";
+import isAuthenticated from "./middlewares/isAuthenticated";
 
 const app: Hono = new Hono();
 const prisma = new PrismaClient();
 
-app.get("/", async (ctx) => {
-    try {
-        const posts = await prisma.post.findMany();
-        return ctx.json({ success: true, data: posts, length: posts.length }, 200);
-    } catch {
-        return ctx.json({ success: false, error: "Failed to fetch posts" }, 500);
-    }
+const postSchema = z.object({
+  content: z.string().min(1),
 });
+
+// すべての投稿を取得 (テスト用)
+app.get("/", async (c) => {
+  try {
+    const posts = await prisma.post.findMany();
+    return c.json({ success: true, data: posts, length: posts.length }, 200);
+  } catch {
+    return c.json({ success: false, error: "Failed to fetch posts" }, 500);
+  }
+});
+
+// IDで指定された投稿を取得
+app.get("/:id", async (c) => {
+  try {
+    const post = await prisma.post.findUnique({
+      where: {
+        id: c.req.param("id"),
+      },
+    });
+    return c.json({ success: true, data: post }, 200);
+  } catch {
+    return c.json({ success: false, error: "Failed to fetch post" }, 500);
+  }
+});
+
+// 投稿を作成
+app.post(
+  "/",
+  isAuthenticated,
+  zValidator("json", postSchema, (result, c) => {
+    if (!result.success) {
+      return c.json({ success: false, error: result.error, data: null }, 400);
+    }
+  }),
+  async (c) => {
+    const { content }: { content: string } = c.req.valid("json");
+    const userId = c.get("jwtPayload").sub;
+    try {
+      const post = await prisma.post.create({
+        data: {
+          content,
+          userId,
+        },
+      });
+      return c.json({ success: true, data: post }, 201);
+    } catch {
+      return c.json(
+        { success: false, error: "Failed to create post", data: null },
+        500
+      );
+    }
+  }
+);
 
 export default app;

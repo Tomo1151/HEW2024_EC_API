883668ee17b1878bb11dc2cf7b580fb9d6f02d5e
Tomo <94550718+Tomo1151@users.noreply.github.com>
2024-11-27 15:20:18 +0900

    products init

--- src/products.ts
+++ src/products.ts
@@ -9,4 +9,38 @@ import isAuthenticated from "./middlewares/isAuthenticated.js";
 const app: Hono = new Hono();
 const prisma = new PrismaClient();
 
+// MARK: スキーマ定義
+
+// MARK: 商品一覧の取得
+app.get("/", async (c) => {
+  try {
+    const products = await prisma.product.findMany();
+    return c.json(
+      { success: true, data: products, length: products.length },
+      200
+    );
+  } catch (e) {
+    console.log(e);
+    return c.json({ success: false, error: "Failed to fetch products" }, 500);
+  }
+});
+
+// MARK: 商品の取得
+app.get("/:id", async (c) => {
+  const id: string = c.req.param("id");
+
+  try {
+    const product = await prisma.product.findUniqueOrThrow({
+      where: { id },
+    });
+    return c.json({ success: true, data: product }, 200);
+  } catch (e) {
+    // console.log(e);
+    return c.json(
+      { success: false, error: "Failed to fetch the product" },
+      500
+    );
+  }
+});
+
 export default app;

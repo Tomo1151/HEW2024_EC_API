9f34c8f294ed78c0828b5136735cd9a043247903
Tomo <94550718+Tomo1151@users.noreply.github.com>
2024-12-28 23:45:24 +0900

    add get /purchase endpoint

--- src/purchases.ts
+++ src/purchases.ts
@@ -14,6 +14,13 @@ const app: Hono = new Hono();
 const prisma = new PrismaClient();
 
 // MARK: スキーマ定義
+
+// 購入履歴の取得GETのスキーマ
+const getPurchaseSchema = z.object({
+  before: z.string(),
+});
+
+// 購入POSTのスキーマ
 const purchaseSchema = z.object({
   productIds: z.array(z.string()),
 });
@@ -40,6 +47,17 @@ app.post(
               in: productIds,
             },
           },
+          include: {
+            post: {
+              include: {
+                author: {
+                  select: {
+                    id: true,
+                  },
+                },
+              },
+            },
+          },
         });
 
         for (const product of products) {
@@ -56,6 +74,13 @@ app.post(
           });
 
           purchases.push(purchase);
+
+          sendNotification({
+            type: NOTIFICATION_TYPES.PURCHASE,
+            senderId: userId,
+            recepientId: product.post.author.id,
+            relPostId: product.post.id,
+          });
         }
 
         return purchases;
@@ -67,6 +92,91 @@ app.post(
       );
     } catch (e) {
       return c.json({ success: false, error: "Failed to like the post" }, 400);
+    }
+  }
+);
+
+// MARK: 購入履歴の取得
+app.get(
+  "/",
+  isAuthenticated,
+  zValidator("query", getPurchaseSchema, (result, c) => {
+    if (!result.success) {
+      return c.json(
+        {
+          success: false,
+          error: result.error,
+        },
+        400
+      );
+    }
+  }),
+  async (c) => {
+    const userId: string = c.get("jwtPayload").sub;
+    const before: string = c.req.valid("query").before;
+
+    try {
+      let targetPurchase;
+      if (before) {
+        targetPurchase = await prisma.purchase.findUniqueOrThrow({
+          where: { id: before },
+          select: { created_at: true },
+        });
+      }
+
+      const purchases = await prisma.purchase.findMany({
+        where: {
+          userId,
+          created_at: targetPurchase
+            ? {
+                lt: targetPurchase.created_at,
+              }
+            : {},
+        },
+        orderBy: {
+          created_at: "desc",
+        },
+        take: 10,
+        select: {
+          id: true,
+          purchase_price: true,
+          created_at: true,
+          product: {
+            select: {
+              id: true,
+              name: true,
+              price: true,
+              thumbnail_link: true,
+              post: {
+                select: {
+                  author: {
+                    select: {
+                      id: true,
+                      username: true,
+                      nickname: true,
+                      icon_link: true,
+                    },
+                  },
+                  id: true,
+                  content: true,
+                  images: {
+                    select: {
+                      image_link: true,
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      });
+
+      return c.json({ success: true, data: purchases }, 200);
+    } catch (e) {
+      return c.json(
+        { success: false, error: "Failed to fetch purchases" },
+        400
+      );
     }
   }
 );

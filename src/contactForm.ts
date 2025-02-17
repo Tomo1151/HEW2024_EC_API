11ef027f9cc75470776cedf5c8faf60505581bcc
kaka_256 <kumarai460@gmail.com>
2025-02-17 22:09:18 +0900

    feat: add contact form

---
+++ src/contactForm.ts
@@ -0,0 +1,55 @@
+import { PrismaClient } from "@prisma/client";
+import { Hono } from "hono";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
+
+// MARK: 定数宣言
+const app: Hono = new Hono();
+
+const contactFormSchema = z.object({
+  name: z.string().min(1),
+  email: z.string().email(),
+  message: z.string().min(1),
+});
+
+app.post(
+  "/",
+  zValidator("json", contactFormSchema, (result, c) => {
+    if (!result.success) {
+      return c.json(
+        {
+          success: false,
+          error: result.error.issues.map((issue) => issue.message),
+        },
+        400
+      );
+    }
+  }),
+  async (c) => {
+    const { name, email, message } = c.req.valid("json");
+    const response = await fetch(
+      "https://discord.com/api/webhooks/1341027640970313791/wzP-KH0iyHkN2P-fgMPHD74qSi6uaBX5UyE1EoatkdxmFLgS8zSLrswq0Q7tFmBjHfWC",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          content: `お名前:\n* ${name} \n\nメール:\n* ${email}\n\n内容:\n* ${message}`,
+        }),
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+    await console.log(await response.text());
+    return c.json(
+      {
+        success: true,
+        data: {
+          name,
+          email,
+          message,
+        },
+      },
+      200
+    );
+  }
+);
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,7 @@ import quotes from "./quotes.js";
 import follows from "./follows.js";
 import notifications from "./notifications.js";
 import stats from "./stats.js";
+import contactForm from "./contactForm.js";
 import { isAllowedOrigin } from "./middlewares/isAllowedOrigin.js";
 
 // const packageJson: { version: string } = require("../package.json");
@@ -76,6 +77,7 @@ app.route("/", likes);
 app.route("/users", follows);
 app.route("/notifications", notifications);
 app.route("/stats", stats);
+app.route("/contactForm", contactForm);
 
 app.get("/", (c) => {
   return c.text("Hello Hono! 🔥");

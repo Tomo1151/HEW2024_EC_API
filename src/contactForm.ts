02f1d0b3230c8a1b2129351edfa1fa785acedc9e
kaka_256 <kumarai460@gmail.com>
2025-02-18 10:40:05 +0900

    fix: save discord webhook url to .env

--- src/contactForm.ts
+++ src/contactForm.ts
@@ -6,6 +6,12 @@ import { zValidator } from "@hono/zod-validator";
 // MARK: 定数宣言
 const app: Hono = new Hono();
 
+if (!process.env.DISCORD_WEBHOOK) {
+  throw new Error("Discord webhook URL isn't defined");
+}
+
+const DISCORD_WEBHOOK: string = process.env.DISCORD_WEBHOOK;
+
 const contactFormSchema = z.object({
   name: z.string().min(1),
   email: z.string().email(),
@@ -27,16 +33,14 @@ app.post(
   }),
   async (c) => {
     const { name, email, message } = c.req.valid("json");
-    const response = await fetch(
-      "https://discord.com/api/webhooks/1341027640970313791/wzP-KH0iyHkN2P-fgMPHD74qSi6uaBX5UyE1EoatkdxmFLgS8zSLrswq0Q7tFmBjHfWC",
-      {
-        method: "POST",
-        body: JSON.stringify({
-          content: `お名前:\n* ${name} \n\nメール:\n* ${email}\n\n内容:\n* ${message}`,
-        }),
-        headers: { "Content-Type": "application/json" },
-      }
-    );
+    const response = await fetch(DISCORD_WEBHOOK, {
+      method: "POST",
+      body: JSON.stringify({
+        content: `お名前:\n* ${name} \n\nメール:\n* ${email}\n\n内容:\n* ${message}`,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
     return c.json(
       {
         success: true,
